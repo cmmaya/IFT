faf0cb404dd65ce3a8246181095e016ee241e507
cmmaya <cmonsalvem@unal.edu.co>
2024-06-24 07:53:24 -0500

    added fulldilled proportion

--- test_data.py
+++ test_data.py
@@ -1,4 +1,4 @@
-from time_dependance_CoW import getWeightProportion, getDataFrameFirstMinutes
+from time_dependence_CoW import getWeightProportion, getDataFrameFirstMinutes
 from optimize_swap import optimizeSwap
 import pandas as pd
 
@@ -6,11 +6,13 @@ import pandas as pd
 df = pd.read_csv('query_result.csv')
 df = df.dropna()
 df2 = pd.read_csv('rainbowSwap.csv')
+MINUTES = 60
 
 # Store the fullfill percentage of CoW
-df_minutes = getDataFrameFirstMinutes(15)
+df_minutes = getDataFrameFirstMinutes(MINUTES)
 CoW_fill_percentage = getWeightProportion(df_minutes)
 
+# Rename columns to run optiize_swap script
 df.rename(columns={'time': 'block_time'}, inplace=True)
 df.rename(columns={'buy_token': 'token_bought_symbol'}, inplace=True)
 df.rename(columns={'sell_token': 'token_sold_symbol'}, inplace=True)
@@ -22,6 +24,13 @@ df.rename(columns={'sell_value_usd': 'amount_usd'}, inplace=True)
 df['price_token_sold']=df['amount_usd']/df['token_sold_amount']
 df['price_token_bought']=df['amount_usd']/df['token_bought_amount']
 
-sol = optimizeSwap(df, 60)
-print(sol.fulfilled_percentage)
+# Get the fullfill percentage of each transaction
+sol = optimizeSwap(df, MINUTES)
+fullfilled_percentage = sol.fulfilled_percentage * 0.01
+last_minutes_df = df[df['block_time'] > df['block_time'].max() - pd.Timedelta(minutes=MINUTES)]
+
+# Get the proportion
+proportion_fulfilled = sum(last_minutes_df['amount_usd'].values * fullfilled_percentage) / sum(last_minutes_df['amount_usd'].values)
+print("IFT fullfilled proportion = ", proportion_fulfilled)
+print("CoW fullfilled proportion = ", CoW_fill_percentage)
 
