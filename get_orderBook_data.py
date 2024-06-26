9d91e688794737c1f731fe3f92afbe763f78268b
cmmaya <111370035+cmmaya@users.noreply.github.com>
2024-06-26 12:51:52 -0500

    Modified get orderbook data to work with new changes

--- get_orderBook_data.py
+++ get_orderBook_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from utils import get_token_info
+from utils import get_token_info, get_eth_price_usd
 import json
 
 # Read JSON data from a text file
@@ -8,24 +8,35 @@ file_path = "JSON_data.txt"
 with open(file_path, "r") as file:
     data = json.load(file)
 
-# Extract the orders
+# Read the additional JSON data for the auction_9051834
+auction_file_path = "auction_9051834.json"
+
+with open(auction_file_path, "r") as auction_file:
+    auction_data = json.load(auction_file)
+
+# Extract the orders and the fulfilled order ids
 orders = data.get("orders", [])
 block_id = data.get("id")
+fulfilled_order_ids = {order.get("id") for solution in auction_data.get("solutions", []) for order in solution.get("orders", [])}
+
+# Get the actual eth price
+eth_price = get_eth_price_usd()
+
 # Prepare the list to hold the extracted data
 data_list = []
 
 # Loop through each order and extract the required fields
 for order in orders:
-    tx_from = order.get("uid")
+    uid = order.get("uid")
     token_bought = order.get("buyToken")
     token_sold = order.get("sellToken")
 
-    token_bought_info = get_token_info(token_bought)
+    token_bought_info = get_token_info(token_bought, block_id, eth_price)
     token_bought_symbol = token_bought_info['symbol']
     token_bought_price_usd = token_bought_info['price_usd']
     # time.sleep(0.35)  # Ensuring rate limit compliance
 
-    token_sold_info = get_token_info(token_sold)
+    token_sold_info = get_token_info(token_sold, block_id, eth_price)
     token_sold_symbol = token_sold_info['symbol']
     token_sold_price_usd = token_sold_info['price_usd']
     # time.sleep(0.35)  # Ensuring rate limit compliance
@@ -34,24 +45,31 @@ for order in orders:
     token_sold_amount = int(order.get("sellAmount"))
 
     # Adjust token amounts based on the token symbol
-    if token_bought_symbol in ['USDC', 'USDT']:
+    if token_bought_symbol and any(substring in token_bought_symbol for substring in ['USDC', 'USDT']):
         token_bought_amount *= 1e-6
+        token_bought_price_usd *= 1e-30
     else:
         token_bought_amount *= 1e-18
+        token_bought_price_usd *= 1e-18
 
-    if token_sold_symbol in ['USDC', 'USDT']:
+    if token_sold_symbol and any(substring in token_sold_symbol for substring in ['USDC', 'USDT']):
         token_sold_amount *= 1e-6
+        token_sold_price_usd *= 1e-30
     else:
         token_sold_amount *= 1e-18
+        token_sold_price_usd *= 1e-18
 
     # Calculate amount_usd
     if token_sold_amount is not None and token_sold_price_usd is not None:
-        amount_usd = int(token_sold_amount) * token_sold_price_usd
+        amount_usd = token_sold_amount * token_sold_price_usd
     else:
         amount_usd = None
-    
+
+    # Determine if the order is fulfilled
+    fulfill = 1 if uid in fulfilled_order_ids else 0
+
     data_list.append({
-        "tx_from": tx_from,
+        "uid": uid,
         "amount_usd": amount_usd,
         "token_bought_symbol": token_bought_symbol,
         "token_sold_symbol": token_sold_symbol,
@@ -59,6 +77,7 @@ for order in orders:
         "token_sold_amount": token_sold_amount,
         "token_bought_price_usd": token_bought_price_usd,
         "token_sold_price_usd": token_sold_price_usd,
+        "fulfill": fulfill
     })
 
 # Convert the list of dictionaries to a pandas DataFrame
