4ca6f494e92d92939a085e26e0a15489e495edae
cmmaya <111370035+cmmaya@users.noreply.github.com>
2024-07-17 19:26:11 -0500

    add metrics to optimization alg

--- test_fullfilled_proportion.py
+++ test_fullfilled_proportion.py
@@ -5,7 +5,7 @@ import time
 import re
 
 # Function to process chunks and collect results
-def process_chunk(chunk_num, df):
+def process_chunk(chunk_num, df, dataset_id):
     start_time_chunk = time.time()  # Start time for the chunk processing
     df_chunk = df.iloc[0:50].copy()
     
@@ -17,23 +17,30 @@ def process_chunk(chunk_num, df):
         # Optimize and calculate fullfilled proportion
         sol = optimizeSwap(df_chunk)
         fullfilled_percentage = sol.fulfilled_percentage * 0.01
-        fullfilled_proportion = sum(fullfilled_percentage.values * df_chunk['amount_usd'].values) / sum(df['amount_usd'].values)
-        total_volume = sum(df['amount_usd'].values)
         volume_paired = sum(fullfilled_percentage.values * df_chunk['amount_usd'].values)
+        fullfilled_proportion = volume_paired / sum(df['amount_usd'].values)
+        total_volume = sum(df['amount_usd'].values)
+        Volume_fee_estimate = volume_paired * 0.0001
+
+        CoW_total_volume = sum(df['fulfill'].values * df['amount_usd'].values)
+        CoW_proportion_volume = CoW_total_volume / sum(df['amount_usd'].values)
         CoW_transaction_paired = sum(df['fulfill'])
         CoW_fee_estimate =  sum(df['fee'])
-        Volume_fee_estimate = volume_paired * 0.01
+
+
         # Return chunk results
         return {
-            'chunk': chunk_num,
+            'auction_id': dataset_id,
             'deals_paired': sum(sol.fulfilled_percentage.values) / 100,
             'fullfilled_proportion': fullfilled_proportion,
             'total_volume': total_volume,
             'volume_paired': volume_paired,
             'execution_time_seconds': time.time() - start_time_chunk,
+            'CoW_total_volume': CoW_total_volume,
+            'CoW_volume_proportion': CoW_proportion_volume,
             'CoW transaction paired': CoW_transaction_paired,
             'CoW_fee_estimate': CoW_fee_estimate,
-            'Volume fee estimate': Volume_fee_estimate
+            'Volume_fee_estimate': Volume_fee_estimate
         }
     except Exception as e:
         print(f"Error occurred in chunk {chunk_num}: {str(e)}")
@@ -54,7 +61,7 @@ def Iterate_over_chunks(df, chunk_size):
         df_chunk = df.iloc[start_idx:end_idx].copy()
         
         # Process chunk and handle errors
-        chunk_result = process_chunk(chunk_num + 1, df_chunk)
+        chunk_result = process_chunk(chunk_num + 1, df_chunk, 0)
         if chunk_result is not None:
             results.append(chunk_result)
         else:
@@ -71,12 +78,12 @@ def iterate_over_datasets(list_of_datasets):
         i += 1
         csv_filename = os.path.join("dataframes", f"dataframe_{dataset}.csv")
         df = pd.read_csv(csv_filename)
-        df = df[df['amount_usd'] != 0]  # Drop rows with no price data
+        df = df.dropna(how='any')
         df = df[df['amount_usd'] <= 1e6]  # Drop rows with dubious data
         df = df[(df['token_sold_amount'] <= 1e6) | (df['token_sold_amount'] >= 1e-3) ]  # Drop rows with dubious data
         df = df[(df['token_bought_amount'] <= 1e6) |( df['token_bought_amount'] >= 1e-3) ]  # Drop rows with dubious data
         # Process chunk and handle errors
-        chunk_result = process_chunk(1, df)
+        chunk_result = process_chunk(1, df, dataset)
         if chunk_result is not None:
             results.append(chunk_result)
         else:
